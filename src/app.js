ecaa19a37b2af22fe0607f2759c4b413aab10b1a
Guilherme <guideoliveira2197@gmail.com>
2026-05-06 21:51:54 -0300

    configurando deploy

--- src/app.js
+++ src/app.js
@@ -15,7 +15,11 @@ const app = express();
 
 app.use(cookieParser());
 
-app.use(cors());
+app.use(cors({
+  origin: "*", 
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
 
 app.use(helmet());
 app.use(limiter);
